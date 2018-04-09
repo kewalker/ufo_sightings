2267eb6ca2c55da18b24b26505fc7c97e6af35da
kewalker <kewalker@uvic.ca>
2018-04-08 23:54:14 -0700

    first bar chart

---
+++ page_load.js
@@ -0,0 +1,50 @@
+var craft_dict = null;
+
+function print_craft_dict () {
+	console.log (craft_dict);
+}
+
+var svg = d3.select("svg"),
+    margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = +svg.attr("width") - margin.left - margin.right,
+    height = +svg.attr("height") - margin.top - margin.bottom;
+
+var x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
+    y = d3.scaleLinear().rangeRound([height, 0]);
+
+var g = svg.append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+d3.csv("data/craft_frequency.csv", function(d) {
+  d.count = +d.count;
+  return d;
+}, function(error, data) {
+  if (error) throw error;
+
+  x.domain(data.map(function(d) { return d.craft_type; }));
+  y.domain([0, d3.max(data, function(d) { return d.count; })]);
+
+  g.append("g")
+      .attr("class", "axis axis--x")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
+
+  g.append("g")
+      .attr("class", "axis axis--y")
+      .call(d3.axisLeft(y).ticks(10))
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", "0.71em")
+      .attr("text-anchor", "end")
+      .text("Frequency");
+
+  g.selectAll(".bar")
+    .data(data)
+    .enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d) { return x(d.craft_type); })
+      .attr("y", function(d) { return y(d.count); })
+      .attr("width", x.bandwidth())
+      .attr("height", function(d) { return height - y(d.count); });
+});
--- parse_data.py
+++ parse_data.py
@@ -1,6 +1,6 @@
 import csv
 
-with open ('scrubbed.csv', newline='') as f:
+with open ('data/scrubbed.csv', newline='') as f:
 	reader = csv.reader(f)
 
 	header = reader.__next__()
@@ -18,6 +18,9 @@ with open ('scrubbed.csv', newline='') as f:
 			temp += 1
 			d[row[4]] = temp
 
+	for row in reader:
+		print (row)
+
 	sorted_tuples = (sorted(d.items(), key=lambda x:x[1]))
 	# print (sorted(d.items(), key=lambda x:x[1]))
 
@@ -28,14 +31,50 @@ with open ('scrubbed.csv', newline='') as f:
 
 	print ('total sightings: {}'. format(sightings))
 
-	_list = []
+	craft_frequency = []
 
 	for tup in sorted_tuples:
-		_list.append([tup[0], tup[1]])
+		craft_frequency.append([tup[0], tup[1]])
 
-	print (_list)
+	print (craft_frequency)
 
-	with open ('craft_frequency.csv', 'w', newline='') as file:
+	with open ('data/craft_frequency.csv', 'w', newline='') as file:
 		writer = csv.writer(file)
 		writer.writerow (['craft_type', 'count'])
-		writer.writerows(_list)
+		writer.writerows(craft_frequency)
+
+with open ('data/scrubbed.csv', newline='') as f:
+	reader = csv.reader(f)
+
+	header = reader.__next__()
+	# print (header)
+
+	d = {}
+
+	for row in reader:
+		if row[3] == '':
+			pass
+		elif row[3] not in d:
+			d[row[3]] = 1
+		else:
+			temp = d[row[3]]
+			temp += 1
+			d[row[3]] = temp
+
+	for row in reader:
+		print (row)
+
+	sorted_tuples = (sorted(d.items(), key=lambda x:x[1]))
+	# print (sorted(d.items(), key=lambda x:x[1]))
+
+	country_frequency = []
+
+	for tup in sorted_tuples:
+		country_frequency.append([tup[0], tup[1]])
+
+	print (country_frequency)
+
+	with open ('data/country_frequency.csv', 'w', newline='') as file:
+		writer = csv.writer(file)
+		writer.writerow (['country', 'count'])
+		writer.writerows(country_frequency)
