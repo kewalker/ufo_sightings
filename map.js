4339ca6616ad7e8f9ed1c7ec74cd3571e341fae9
kewalker <kewalker@uvic.ca>
2018-04-10 23:18:58 -0700

    plots points on map

--- map.js
+++ map.js
@@ -2,9 +2,9 @@ var widthmap = 960,
     heightmap = 500;
 
 var projection = d3.geoMercator()
-    .center([0, 5 ])
-    .scale(200)
-    .rotate([-180,0]);
+    // .center([0, 5 ])
+    // .scale(200)
+    // .rotate([-180,0]);
 
 var map = d3.select("#map").append("svg")
     .attr("width", widthmap)
@@ -19,27 +19,27 @@ var path = d3.geoPath()
 var g2 = map.append("g");
 
 // load and display the World
-d3.json("data/world-50m.json", function(error, topology) {
+d3.json("data/world-110m2.json", function(error, topology) {
 
 // load and display the cities
-// d3.csv("cities.csv", function(error, data) {
-//     g.selectAll("circle")
-//        .data(data)
-//        .enter()
-//        .append("a")
-//           .attr("xlink:href", function(d) {
-//             return "https://www.google.com/search?q="+d.city;}
-//           )
-//        .append("circle")
-//        .attr("cx", function(d) {
-//                return projection([d.lon, d.lat])[0];
-//        })
-//        .attr("cy", function(d) {
-//                return projection([d.lon, d.lat])[1];
-//        })
-//        .attr("r", 5)
-//        .style("fill", "red");
-// });
+d3.csv("data/map_subset.csv", function(error, data) {
+    g2.selectAll("circle")
+       .data(data)
+       .enter()
+       .append("a")
+          .attr("xlink:href", function(d) {
+            return "https://www.google.com/search?q="+d.city;}
+          )
+       .append("circle")
+       .attr("cx", function(d) {
+               return projection([d.longitude, d.latitude])[0];
+       })
+       .attr("cy", function(d) {
+               return projection([d.longitude, d.latitude])[1];
+       })
+       .attr("r", 5)
+       .style("fill", "red");
+});
 
 g2.selectAll("path")
       .data(topojson.object(topology, topology.objects.countries)
@@ -59,6 +59,7 @@ var zoom = d3.zoom()
         // g2.selectAll("path")  
         //     .attr("d", path.projection(projection)); 
         g2.attr("transform", d3.event.transform)
+
 
   });
 
